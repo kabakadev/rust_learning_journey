6343bcb12fcd3a932e8a4fcfa25a933e9781cbad
iankabaka <iankabaka1@gmail.com>
2026-01-10 13:32:36 +0300

    traits

---
+++ chapter_10_02_traits/src/main.rs
@@ -0,0 +1,27 @@
+use chapter_10_02_traits::{SocialPost,NewsArticle, Summary};
+
+fn main() {
+    let post = SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        repost: false,
+    };
+
+    println!("1 new post: {}", post.summarize());
+
+        let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+}
