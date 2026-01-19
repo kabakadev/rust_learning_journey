c218ae3cb93fff5d48c3426c93a9971fcd93e706
iankabaka <iankabaka1@gmail.com>
2026-01-19 15:37:02 +0300

    ownership

--- chapter_04_01_what_is_ownership/src/main.rs
+++ chapter_04_01_what_is_ownership/src/main.rs
@@ -1,17 +1,12 @@
 fn main(){
-    let s = String::from("hello");
-    take_ownership(s);
+    let mut s = String::from("hello");
+    //scope 1: the writer works alone
+    let r1 = &mut s;
+    r1.push_str(", world"); //
+    println!("{}", r1);
+    //r1 is no longer used after this, so its "lock" on is released
 
-    let x = 5;
-
-    makes_copy(x);
-
-}
-
-fn take_ownership(some_string:String){
-    println!("{some_string}");
-}
-
-fn makes_copy(some_integer: i32){
-    println!("{some_integer}");
+    //scope 2: the reader comes in afterwards
+    let r2 = &s;
+    println!("{}", r2);
 }
